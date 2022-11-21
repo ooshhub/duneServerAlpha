2659650ab4f211c8229ce60b7883999034981026
ooshhub <ooshp@internode.on.net>
2022-11-21 22:38:02 +1030

    More updating the things

--- src/server/main.ts
+++ src/server/main.ts
@@ -11,8 +11,10 @@ import readLine from "readline/promises";
  * TODO: create event system for server.... localHub should do it?
  *  - simple event routing to handle player ids / houses etc. and route the traffic to the right place
  *  - game core only cares about houses
- * 	- server cares about sockets
+ * 	- socket.io server only cares about sockets
  *  - WhoIs service takes care of translating playerId/houseId/socketId
+ *  - WhoIs service sits between socket layer and game core
+ *  - All other house functions are internal to game core
  * 
  * TODO: server functions for the following
  *  - wrapper function for async request with retries for requesting data from players
@@ -27,10 +29,12 @@ import readLine from "readline/promises";
 
 // Spin up a server when given port number
 	// TODO: rewrite socketserver
+	// careful where you put your handlers when spinning up a server, dickhead
 
 // Listen for players joining
 
-// Player [localhost] is the host.
+// Player [localhost] is always host when server is in self-host mode
+// host-passing doesn't matter until remote deploy happens
 
 // Player host is the source of truth until a lobby in finalised, then the server is boss.
 	// TODO: rewrite lobby functionality
@@ -38,68 +42,73 @@ import readLine from "readline/promises";
 // Start the game. Play. Win. Coding complete.
 
 
-const serverCommands = (() => {
-
-	const STATUS_MARKER = `%STATUS%`;
-
-	const echo = (inputString: string) => {
-		const output = inputString.replace(/^%\w+%/, '');
-		process.stdout.write(`${STATUS_MARKER}${output}`);
-	}
-
-	const pong = () => {
-		process.stdout.write(`${STATUS_MARKER}pong`);
-	}
-
-	const log = (message: string) => {
-		message = message.replace(/^\s*%\w+%/, '');
-		logger.log(message);
-	}
-
-	return { echo, pong, log }
-
-})();
-
-const logger = new Logger('CuntyLogger');
 
 
-const mockServer = (async () => {
 
-	await logger.init();
+/**
+ * TEMP STUFF BELOW IS PROBABLY JUNK
+ */
 
-	const STATUS_MARKER = `%STATUS%`;
 
-	const COMMANDS: { [key: string]: (message: string) => void } = {
-		"ECHO": serverCommands.echo,
-		'PING': serverCommands.pong,
-		'LOG':	serverCommands.log
-	}
+// const serverCommands = (() => {
 
-	const exitServer = () => {
-		logger?.destroy();
-		process.exit();
-	}
+// 	const STATUS_MARKER = `%STATUS%`;
 
-	const stdInReader = readLine.createInterface({
-		input: stdin,
-		output: stdout,
-	});
-	stdInReader.on('line', async (message) => {
-		message = message.trim();
-		const isCommand = message.match(/^%(\w+)%/);
-		if (isCommand) {
-			const command = `${isCommand[1]}`;
-			if (Reflect.has(COMMANDS, command)) {
-				COMMANDS[command](message);
-				return;
-			}
-		}
-		else process.stdout.write(`Server knows not this command.`);
-	});
+// 	const echo = (inputString: string) => {
+// 		const output = inputString.replace(/^%\w+%/, '');
+// 		process.stdout.write(`${STATUS_MARKER}${output}`);
+// 	}
 
-	await logger.warn('Fucking shit eh');
-	process.stdout.write(`${STATUS_MARKER}Server is online`);
+// 	const pong = () => {
+// 		process.stdout.write(`${STATUS_MARKER}pong`);
+// 	}
+
+// 	const log = (message: string) => {
+// 		message = message.replace(/^\s*%\w+%/, '');
+// 		logger.log(message);
+// 	}
+
+// 	return { echo, pong, log }
+
+// })();
+
+// const mockServer = (async () => {
+
+// 	await logger.init();
+
+// 	const STATUS_MARKER = `%STATUS%`;
+
+// 	const COMMANDS: { [key: string]: (message: string) => void } = {
+// 		"ECHO": serverCommands.echo,
+// 		'PING': serverCommands.pong,
+// 		'LOG':	serverCommands.log
+// 	}
+
+// 	const exitServer = () => {
+// 		logger?.destroy();
+// 		process.exit();
+// 	}
+
+// 	const stdInReader = readLine.createInterface({
+// 		input: stdin,
+// 		output: stdout,
+// 	});
+// 	stdInReader.on('line', async (message) => {
+// 		message = message.trim();
+// 		const isCommand = message.match(/^%(\w+)%/);
+// 		if (isCommand) {
+// 			const command = `${isCommand[1]}`;
+// 			if (Reflect.has(COMMANDS, command)) {
+// 				COMMANDS[command](message);
+// 				return;
+// 			}
+// 		}
+// 		else process.stdout.write(`Server knows not this command.`);
+// 	});
+
+// 	await logger.warn('Fucking shit eh');
+// 	process.stdout.write(`${STATUS_MARKER}Server is online`);
 	
-});
+// });
 
-await mockServer();
+// await mockServer();
